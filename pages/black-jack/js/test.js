f7d19e0820810757aecfca6df86a18f5d0914e5d
lmisterek <leeanna.misterek@gmail.com>
2023-05-05 09:38:07 -0700

    Delete pages directory

--- pages/black-jack/js/test.js
+++
@@ -1,6 +0,0 @@
-/* Test VALUES */
-var player = new Player("player-hand", {'suit': 'hearts', 'position': '10'}, 
-			{'suit': 'diamonds', 'position': '10'});
-
-var dealer = new Player("player-hand", {'suit': 'spades', 'position': '3'}, 
-			{'suit': 'clubs', 'position': '3'});
